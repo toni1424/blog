15e2b1c5a50255e9d5298cab33f1529f0d351dfa
AlexNeil7 <indizx@gmail.com>
2016-05-18 01:33:57 +0300

    %commit_message%

--- models.py
+++ models.py
@@ -1,14 +1,16 @@
 from django.contrib.auth.models import User
 from django.db import models
+# from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+# from django.views.generic.list import ListView
 
-SHORT_TEXT_LEN = 1000
+SHORT_TEXT_LEN = 600
+
 
 class Article(models.Model):
     title = models.CharField(max_length=200)
     text = models.TextField()
     user = models.ForeignKey(User)
     datetime = models.DateTimeField(u'Дата публикации')
-
 
     def __str__(self):
         return self.title
--- views.py
+++ views.py
@@ -1,17 +1,18 @@
-from datetime import timezone
-
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import render, get_object_or_404
 from blog.models import Article
 
+
 def home(request):
-    articles = Article.objects.all()
+    articles = Article.objects.all().order_by('datetime').reverse()
     context = {
         'articles': articles
     }
     return render(request, 'blog/home.html', context)
 
+
 def about(request):
     return render(request, 'blog/about.html')
+
 
 def show_article(requset, article_id):
     article = get_object_or_404(Article, id=article_id)
